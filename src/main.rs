f685b208dcbc624ad66f7b408712fbf4b511a58a
nikicivan <ivan.nikic@gmail.com>
2024-10-29 11:48:54 +0100

    Print number of tables

--- src/main.rs
+++ src/main.rs
@@ -17,17 +17,21 @@ fn main() -> Result<()> {
         ".dbinfo" => {
             let mut file = File::open(&args[1])?;
             let mut header = [0; 100];
+            let mut bheader = [0; 8];
             file.read_exact(&mut header)?;
+            file.read_exact(&mut bheader)?;
 
             // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
             #[allow(unused_variables)]
             let page_size = u16::from_be_bytes([header[16], header[17]]);
+            let number_of_tables = u16::from_be_bytes([bheader[3], bheader[4]]);
 
             // You can use print statements as follows for debugging, they'll be visible when running tests.
             eprintln!("Logs from your program will appear here!");
 
             // Uncomment this block to pass the first stage
             println!("database page size: {}", page_size);
+            println!("number of tables: {}", number_of_tables);
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
